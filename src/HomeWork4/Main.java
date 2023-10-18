4a259c5a77da831fa645d1c5d83c42cc22ae76f7
Elizaveta <al5xeevaliz@yandex.ru>
2023-10-18 18:36:53 +0300

    homework4

---
+++ src/HomeWork4/Main.java
@@ -0,0 +1,21 @@
+package HomeWork4;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassOfTeachers<Teacher> teachers = new ClassOfTeachers<>("teacherss");
+        teachers.add(new ChemistryTeacher("Peter", "Ivanych"));
+        teachers.add(new ShopTeacher("Agafiya", "Sergeevna"));
+
+        for (int index = 0; index < teachers.count(); index++) {
+            System.out.println(teachers.get(index));
+        }
+
+
+
+
+    }
+}
